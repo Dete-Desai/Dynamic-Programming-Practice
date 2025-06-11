e93625782302b952289a86a703c3efaccf6bfe5c
lamech.dete <lamech.dete@smartapplicationsgroup.com>
2025-06-11 07:16:20 +0300

    updates on interleaving_string_problemm slutions

--- algorithm_problems/interleaving_string_problem.py
+++ algorithm_problems/interleaving_string_problem.py
@@ -58,3 +58,27 @@ def inter(s1, s2, s3, i=0, j=0, lookup=None):
     else:
         lookup[(i, j)] = (i < len(s1) and s1[i] == s3[i + j] and inter(s1, s2, s3, i + 1, j, lookup) or j < len(s2) and s2[j] == s3[i + j] and inter(s1, s2, s3, i, j + 1, lookup))
         return lookup[(i, j)]
+    
+# SOLUTION 2
+
+# T(n,m) = O(nm)
+# S(n,m) = O(m)
+
+# def inter(s1, s2, s3):
+#     n, m = len(s1), len(s2)
+#     if n+m != len(s3):
+#         return False
+#     prev_dp = [False]*(m+1)
+#     dp = [False]*(m+1)
+#     prev_dp[0] = True
+#     for j in range(1, m+1):
+#         prev_dp[j] = s2[j-1] == s3[j-1] and prev_dp[j-1]
+#     for i in range(1, n+1):
+#         dp[0] = s1[i-1] == s3[i-1] and prev_dp[0]
+#         for j in range(1, m+1):
+#             check_s1 = s1[i-1] == s3[i+j-1] and prev_dp[j]
+#             check_s2 = s2[j-1] == s3[i+j-1] and dp[j-1]
+#             dp[j] = check_s1 or check_s2
+#         prev_dp = dp
+#         dp = [False]*(m+1)
+#     return prev_dp[m]
