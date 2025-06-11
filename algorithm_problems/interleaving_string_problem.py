d524fc4acda9b73c26845e8c8be42a05467597f7
lamech.dete <lamech.dete@smartapplicationsgroup.com>
2025-06-11 06:55:31 +0300

    updates on interleaving_string_problem solution 1

--- algorithm_problems/interleaving_string_problem.py
+++ algorithm_problems/interleaving_string_problem.py
@@ -37,8 +37,24 @@
 
 # Return type: bool
 
+# SOLUTION 1
+
 # T(n,m) = O(nm)
 # S(n,m) = O(nm)
 
-def inter(s1, s2, s3):
-    pass
+def inter(s1, s2, s3, i=0, j=0, lookup=None):
+    
+    lookup = {} if lookup is None else lookup
+    
+    if (i, j) in lookup:
+        return lookup[(i, j)]
+        
+    if len(s1) + len(s2) != len(s3):
+        return False
+        
+    elif i == len(s1) and j == len(s2):
+        return True
+        
+    else:
+        lookup[(i, j)] = (i < len(s1) and s1[i] == s3[i + j] and inter(s1, s2, s3, i + 1, j, lookup) or j < len(s2) and s2[j] == s3[i + j] and inter(s1, s2, s3, i, j + 1, lookup))
+        return lookup[(i, j)]
