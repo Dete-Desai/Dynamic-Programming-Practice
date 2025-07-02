96780b8cb5d8ac56fcbdc3b3658faa4fc3da6064
lamech.dete <lamech.dete@smartapplicationsgroup.com>
2025-07-02 06:20:34 +0300

    matrix chain problem solution

--- algorithm_problems/matrix_chain_problem.py
+++ algorithm_problems/matrix_chain_problem.py
@@ -55,4 +55,19 @@
 # S(n) = O(n^2)
 
 def chain(matrices):
-    pass
+    n = len(matrices)
+    dp = [[0]*n for i in range(n)]
+    for d in range(1, n):
+        for i in range(n-d):
+            j = i+d
+            min_cost = float("inf")
+            
+            for k in range(i,j):
+                left_cost = dp[i][k]
+                right_cost = dp[k+1][j]
+                product_cost = matrices[i][0] * matrices[k][1] * matrices[j][1]
+                
+                min_cost = min(min_cost, left_cost + right_cost + product_cost)
+                
+            dp[i][j] = min_cost
+    return dp[0][n-1]
